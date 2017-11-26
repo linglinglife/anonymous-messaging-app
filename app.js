3ef821ace36d9a2dbc69263f0d519f9988cae520
linglinglife <tongxling@gmail.com>
2017-11-26 20:26:40 +1100

    Add new element

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-function changeText(id) {
-  document.body.appendChild(id);
+function changeText(parameter) {
 
+  var newElement = document.createElement("p")
+  newElement.id="newElement";
+  newElement.innerHTML = document.getElementById('message').value;
+  //document.body.appendChild(newElement);
 
-    document.write("Hello Submit");
+  var lastMessage = document.getElementById('user_message');
+  lastMessage.parentNode.insertAfter(newElement, lastMessage);
+
+  parameter.preventDefault;
+
+  //document.body.appendChild(parameter);
+
+    //document.write(parameter);
     //id.innerHTML = "Ooops!";
 }

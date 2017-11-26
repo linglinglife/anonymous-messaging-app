d574ca9e35cb628bbb320e111487d224ed9694c6
linglinglife <tongxling@gmail.com>
2017-11-26 20:46:00 +1100

    Clear input after submit

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ function changeText(parameter) {
   var newElement = document.createElement("p")
   newElement.id="newElement";
   newElement.innerHTML = document.getElementById('message').value;
+  document.getElementById('message').value = '';
   //document.body.appendChild(newElement);
 
   var messageList = document.getElementById('user_message');
-  messageList.insertBefore(newElement, messageList.childNodes[0]); 
+  messageList.insertBefore(newElement, messageList.childNodes[0]);
 
   parameter.preventDefault;
 

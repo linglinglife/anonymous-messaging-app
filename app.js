097d58ae58c957164571068109d9c3673291fc56
linglinglife <tongxling@gmail.com>
2017-11-29 20:28:54 +1100

    Code cleanup and Load messages from firebase

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-function changeText(parameter) {
+// this function gets executed when the user clicks submit on the page
+function submitNewMessage(submitButton) {
 
+  //addNewMessageToPage(document.getElementById("theNewMessageTheUserPutIntoTheTextField").value);
+
+  pushNewMessageToFirebase(document.getElementById("theNewMessageTheUserPutIntoTheTextField").value);
+
+  // clear user's input field
+  document.getElementById("theNewMessageTheUserPutIntoTheTextField").value = "";
+
+  // avoid page reload on form submission
+  submitButton.preventDefault;
+}
+
+// function to add a new message at the top of the message list on the page
+function addNewMessageToPage(newMessage) {
   var newElement = document.createElement("p")
   newElement.id="newElement";
-  newElement.innerHTML = document.getElementById("message").value;
-  document.getElementById("message").value = "";
-  //document.body.appendChild(newElement);
+  newElement.innerHTML = newMessage;
 
   var messageList = document.getElementById("user_message");
   messageList.insertBefore(newElement, messageList.childNodes[0]);
-
-  parameter.preventDefault;
-
-  submitClick(newElement.innerHTML);
-
-  //document.body.appendChild(parameter);
-
-    //document.write(parameter);
-    //id.innerHTML = "Ooops!";
 }
 
 var message = document.getElementById("message");
 var submitBtn = document.getElementById("submitBtn");
 
-function submitClick(messageText) {
+// Push a new message into firebase
+function pushNewMessageToFirebase(messageText) {
   console.log("Firebase: " + firebase)
 
   let messagesRef = firebase.database().ref("messages");
@@ -31,3 +35,10 @@ function submitClick(messageText) {
   //firebaseRef.child("message").set(messageText);
 
 }
+
+window.onload = function() {
+  var messagesRef = firebase.database().ref("messages");
+  messagesRef.on('child_added', function(data) {
+    addNewMessageToPage(data.val());
+  });
+};
